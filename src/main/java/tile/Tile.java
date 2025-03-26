db7b05eb64518c69b7d6f80848c0762156bf062b
Mateo Iturmendi <s225095@student.dtu.dk>
2025-03-26 14:37:33 +0100

    Made the game lag and made tile enum

--- src/main/java/tile/Tile.java
+++ src/main/java/tile/Tile.java
@@ -1,9 +1,24 @@
 package tile;
 
-import java.awt.image.BufferedImage;
 
-public class Tile {
+public enum Tile {
+    GRASS("/tiles/grass.png", true),
+    STONE("/tiles/stone.png", true),
+    WATER("/tiles/water.png", false);
 
-    public BufferedImage image;
-    public boolean collision = false;
+    private String pictureFile;
+    private boolean walkable;
+
+    private Tile(String pictureFile, boolean isWalkable) {
+        this.pictureFile = pictureFile;
+        this.walkable = isWalkable;
+    }
+
+    public String getPictureFile() {
+        return pictureFile;
+    }
+
+    public boolean isWalkable() {
+        return walkable;
+    }
 }
--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -4,6 +4,7 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,85 +13,30 @@ import java.io.InputStreamReader;
 public class TileManager {
 
     GamePanel gp;
-    Tile[] tile;
-    int mapTileNum[][];
+    Tile[][] mapTiles;
 
     public TileManager(GamePanel gp) {
 
         this.gp = gp;
 
-        tile = new Tile[15];
-        mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
+        mapTiles = new Tile[gp.maxWorldCol][gp.maxWorldRow];
 
-        getTileImage();
-        loadMap("/maps/map02.txt");
+        loadMap();
     }
 
-    public void getTileImage(){
-
-        try{
-
-            tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
-
-            tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/stone.png"));
-
-            tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirt.png"));
-
-            tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/sand.png"));
-
-            tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/snow.png"));
-
-            tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/plank.png"));
-
-            tile[6] = new Tile();
-            tile[6].image = ImageIO.read(getClass().getResourceAsStream("/tiles/log.png"));
-
-            tile[7] = new Tile();
-            tile[7].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
-
-            tile[8] = new Tile();
-            tile[8].image = ImageIO.read(getClass().getResourceAsStream("/tiles/lava.png"));
-
-            tile[9] = new Tile();
-            tile[9].image = ImageIO.read(getClass().getResourceAsStream("/tiles/tree.png"));
-
-            tile[10] = new Tile();
-            tile[10].image = ImageIO.read(getClass().getResourceAsStream("/tiles/chest.png"));
-
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void loadMap(String filePath) {
+    public void loadMap() {
         try {
-            InputStream is = getClass().getResourceAsStream(filePath);
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
             int row = 0;
             // Read one line for each row in your map.
             while (row < gp.maxWorldRow) {
-                String line = br.readLine();
-                if (line == null) {
-                    // End of file reached before filling the expected map rows.
-                    break;
-                }
-
-                String[] numbers = line.split(" ");
                 for (int col = 0; col < gp.maxWorldCol; col++) {
-                    int num = Integer.parseInt(numbers[col]);
-                    // Note: Ensure that your mapTileNum array is indexed the same way as you expect.
-                    mapTileNum[col][row] = num;
+                    mapTiles[col][row] = Tile.GRASS;
                 }
                 row++;
             }
-            br.close();
+
+            System.out.println(mapTiles);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -105,7 +51,7 @@ public class TileManager {
 
         while (worldCol<gp.maxWorldCol && worldRow<gp.maxWorldRow){
 
-            int tileNum = mapTileNum[worldCol][worldRow];
+            Tile tileNum = mapTiles[worldCol][worldRow];
 
             int worldX = worldCol * gp.tileSize;
             int worldY = worldRow * gp.tileSize;
@@ -117,7 +63,16 @@ public class TileManager {
                 worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
                 worldY - gp.tileSize < gp.player.worldY + gp.player.screenY){
 
-                g2.drawImage(tile[tileNum].image,screenX,screenY, gp.tileSize,gp.tileSize,null);
+
+                try{
+                    BufferedImage image = ImageIO.read(getClass().getResourceAsStream(tileNum.getPictureFile()));
+
+                    g2.drawImage(image,screenX,screenY, gp.tileSize,gp.tileSize,null);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
             worldCol++;
 
