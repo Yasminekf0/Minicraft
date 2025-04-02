85c2659dec42103eaa384025f0637fbd3c39ef85
LilMirts <mairo.trump@gmail.com>
2025-04-02 13:56:19 +0200

    Added some functions to ChunkPosition

--- src/main/java/world/position/ChunkPosition.java
+++ src/main/java/world/position/ChunkPosition.java
@@ -5,4 +5,13 @@ public class ChunkPosition extends Position<Integer> {
     public ChunkPosition(Integer x, Integer y) {
         super(x, y);
     }
+
+    public WorldPosition getTopLeftCoords(){
+        return new WorldPosition(16*x, 16*y);
+    }
+
+    public WorldPosition getWorldPosInChunk(double localX, double localY){
+        return new WorldPosition(x*16 + localX, y*16 + localY);
+    }
+
 }
