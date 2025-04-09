42dc29409b509c09ed4ef699c8e76027d3eab93b
LilMirts <mairo.trump@gmail.com>
2025-04-09 15:45:08 +0200

    Added block rendering

--- src/main/java/view/WorldView.java
+++ src/main/java/view/WorldView.java
@@ -1,11 +1,16 @@
 package view;
 
+import io.cucumber.java.hu.Ha;
 import model.entity.Player;
+import model.world.Block;
 import model.world.Tile;
 import model.world.World;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
 
 import static view.ScreenSettings.*;
 
@@ -14,11 +19,15 @@ public class WorldView extends GameElementView {
     private World world;
     private Player player;
     private Tile[][] tiles;
+    private HashMap<Tile, BufferedImage> tileImageMap;
+    private HashMap<Block, BufferedImage> blockImageMap;
 
     public WorldView(World world, Player player) {
         this.world = world;
         this.player = player;
         tiles = world.getMap();
+        loadTiles();
+        loadBlocks();
     }
 
     @Override
@@ -40,11 +49,43 @@ public class WorldView extends GameElementView {
                 int screenY = worldY - playerWorldY + playerScreenY;
 
                 if (i>=0 & i<size & j>=0 & j<size) {
-                    BufferedImage img = tiles[i][j].getImage();
-                    g2.drawImage(img, screenX, screenY, tileSize, tileSize, null);
+                    BufferedImage tileImg = tileImageMap.get(tiles[i][j]);
+                    g2.drawImage(tileImg, screenX, screenY, tileSize, tileSize, null);
+                    if (tiles[i][j].getBlock() != null) {
+                        BufferedImage blockImg = blockImageMap.get(tiles[i][j].getBlock());
+                        g2.drawImage(blockImg, screenX, screenY, tileSize, tileSize, null);
+                    }
                 }
             }
         }
+    }
+
+    private void loadTiles() {
+        tileImageMap = new HashMap<>();
+        tileImageMap.put(Tile.GRASS,getBufferedImage("/tiles/grass.png"));
+        tileImageMap.put(Tile.DIRT,getBufferedImage("/tiles/dirt.png"));
+        tileImageMap.put(Tile.STONE,getBufferedImage("/tiles/stone.png"));
+        tileImageMap.put(Tile.WATER,getBufferedImage("/tiles/water.png"));
+        tileImageMap.put(Tile.LAVA,getBufferedImage("/tiles/lava.png"));
+        tileImageMap.put(Tile.SAND,getBufferedImage("/tiles/sand.png"));
+        tileImageMap.put(Tile.SNOW,getBufferedImage("/tiles/snow.png"));
+    }
+
+    private void loadBlocks() {
+        blockImageMap = new HashMap<>();
+        blockImageMap.put(Block.Wood,getBufferedImage("/tiles/plank.png"));
+        blockImageMap.put(Block.Tree,getBufferedImage("/tiles/tree.png"));
+        blockImageMap.put(Block.Rock,getBufferedImage("/tiles/breaking.png"));
+        blockImageMap.put(Block.Chest,getBufferedImage("/tiles/chest.png"));
+    }
+
+    private BufferedImage getBufferedImage(String path){
+        try {
+            return ImageIO.read(getClass().getResourceAsStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private int[] getTopLeftTileCoords(){
