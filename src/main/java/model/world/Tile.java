60455c958ecb0a1f16e64619316646c80acb4230
Trilex214 <lexvancauter@gmail.com>
2025-04-10 22:31:29 +0200

    Updated Tile Enum

--- src/main/java/model/world/Tile.java
+++ src/main/java/model/world/Tile.java
@@ -5,34 +5,26 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public enum Tile {
-    GRASS("/tiles/grass.png", true),
-    DIRT("/tiles/dirt.png", true),
-    STONE("/tiles/stone.png", true),
-    WATER("/tiles/water.png", false),
-    LAVA("/tiles/lava.png", false),
-    SAND("/tiles/sand.png", true),
-    SNOW("/tiles/snow.png", true);
+    GRASS( true),
+    DIRT( true),
+    STONE(true),
+    WATER( false),
+    LAVA( false),
+    SAND( true),
+    SNOW( true);
 
-    private final String pictureFile;
+
     private final boolean isBlockPlaceable;
 
     private Block block;
 
-    private BufferedImage image;
 
-    Tile(String pictureFile, boolean isBlockPlaceable) {
-        this.pictureFile = pictureFile;
+
+    Tile(boolean isBlockPlaceable) {
         this.isBlockPlaceable = isBlockPlaceable;
-        try {
-            this.image = ImageIO.read(getClass().getResourceAsStream(this.pictureFile));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    public BufferedImage getImage() {
-        return image;
-    }
+
 
     public void setBlock(Block block) {
         this.block = block;
