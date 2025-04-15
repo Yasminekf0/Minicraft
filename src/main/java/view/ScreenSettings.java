45f8bc0f7312ef6f7c6432bc199964dc445ac0a1
Mateo <s225095@dtu.dk>
2025-04-15 13:33:52 +0200

    Fixed Screen Settings

--- src/main/java/view/ScreenSettings.java
+++ src/main/java/view/ScreenSettings.java
@@ -3,10 +3,10 @@ package view;
 public class ScreenSettings {
     // Screen settings
     final static int originalTileSize = 16; // 16x16 px
-    final static int scale = 1;
+    final static int scale = 2;
     public final static int tileSize = originalTileSize * scale; // 48x48 px
-    final static int maxScreenCol = 150;
-    final static int maxScreenRow = 80;
+    final static int maxScreenCol = 30;
+    final static int maxScreenRow = 30;
     static final int screenWidth = tileSize * maxScreenCol;
     static final int screenHeight = tileSize * maxScreenRow;
 
