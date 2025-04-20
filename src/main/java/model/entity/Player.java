3dc79e9824d5599499af7ee2ee6bdd9d00b7e599
Trilex214 <lexvancauter@gmail.com>
2025-04-20 21:28:23 +0200

    Fixed player movement related to scale.

--- src/main/java/model/entity/Player.java
+++ src/main/java/model/entity/Player.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 
 import static java.lang.Math.round;
 import static model.world.WorldSettings.worldSize;
+import static view.ScreenSettings.scale;
 import static view.ScreenSettings.tileSize;
 
 public class Player extends Entity {
@@ -50,10 +51,10 @@ public class Player extends Entity {
     public void moveUntil(double dx, double dy) {
         worldPos.updateDirection(dx,dy);
         for (int x = 0; x<speed; x++) {
-            if (world.hasBlock(worldPos.getTileXPos(),worldPos.getNextYTilePos( round(dy)*20))) dy = 0;
+            if (world.hasBlock(worldPos.getTileXPos(),worldPos.getNextYTilePos( round(dy)*4*scale))) dy = 0;
             else if (!(world.isWalkable(worldPos.getTileXPos(),worldPos.getNextYTilePos(dy)))) dy = 0;
 
-            if (world.hasBlock(worldPos.getNextXTilePos( round(dx)*20),worldPos.getTileYPos())) dx = 0;
+            if (world.hasBlock(worldPos.getNextXTilePos( round(dx)*4*scale),worldPos.getTileYPos())) dx = 0;
             else if (!(world.isWalkable(worldPos.getNextXTilePos(dx),worldPos.getTileYPos()))) dx = 0;
 
             worldPos.increment(dx, dy);
