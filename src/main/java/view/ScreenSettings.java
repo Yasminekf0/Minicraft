1f5edf158bd1aec4697ec0c7e2a8374d9acffa9c
Mateo <s225095@dtu.dk>
2025-04-23 11:02:15 +0200

    DEFAULT SCREEN SETTINGS DON'T CHANGE

--- src/main/java/view/ScreenSettings.java
+++ src/main/java/view/ScreenSettings.java
@@ -1,12 +1,13 @@
 package view;
 
 public class ScreenSettings {
+
     // Screen settings
-    final static int originalTileSize = 16; // 16x16 px
-    public final static int scale = 2;
-    public final static int tileSize = originalTileSize * scale; // 48x48 px
-    final static int maxScreenCol = 30;
-    final static int maxScreenRow = 30;
+    final static int originalTileSize = 16;
+    public final static int scale = 4;
+    public final static int tileSize = originalTileSize * scale;
+    final static int maxScreenCol = 17;
+    final static int maxScreenRow = 17;
     static final int screenWidth = tileSize * maxScreenCol;
     static final int screenHeight = tileSize * maxScreenRow;
 
