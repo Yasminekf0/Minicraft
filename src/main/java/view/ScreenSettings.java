fbc678e1e0b67a0fffacb977d2f0ff263baa28d6
Mateo <s225095@dtu.dk>
2025-04-24 19:05:14 +0200

    DONT CHANGE THE DAMN SCREEN SETTINGS :)

--- src/main/java/view/ScreenSettings.java
+++ src/main/java/view/ScreenSettings.java
@@ -3,10 +3,10 @@ package view;
 public class ScreenSettings {
     // Screen settings
     final static int originalTileSize = 16; // 16x16 px
-    public final static int scale = 2;//4;
+    public final static int scale = 4;//4;
     public final static int tileSize = originalTileSize * scale; // 48x48 px
-    final static int maxScreenCol = 30;//14;
-    final static int maxScreenRow = 30;//14;
+    final static int maxScreenCol = 17;//14;
+    final static int maxScreenRow = 17;//14;
     static final int screenWidth = tileSize * maxScreenCol;
     static final int screenHeight = tileSize * maxScreenRow;
 
