ec826b319f4ec3542f013c0a802088b31e6f80a5
Stefan Pizlo <s234689@dtu.dk>
2025-05-03 19:04:21 +0200

    LoadManager-framework

---
+++ src/test/java/Items/ToolsSteps.java
@@ -0,0 +1,60 @@
+package Items;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import model.Inventory;
+import model.entity.Player;
+import model.items.tools.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ToolsSteps {
+
+    Tool tool;
+    String initialMaterial;
+
+    @Given("that the player has a {string} in {string} section in their inventory")
+    public void thatThePlayerHasAInSectionInTheirInventory(String toolName, String section) {
+        Player player = Player.getInstance();
+        Inventory inventory = player.getInventory();
+        inventory.setCurrentSection(section);
+
+        switch (toolName.toLowerCase()) {
+            case "axe" -> tool = new Axe();
+            case "pickaxe" -> tool = new Pickaxe();
+            case "sword" -> tool = new Sword();
+            default -> throw new IllegalArgumentException("Unsupported tool: " + toolName);
+        }
+
+        inventory.addItem(tool);
+        inventory.setSelectedItem(tool);
+
+        initialMaterial = tool.getMaterial(); // Store material as string
+    }
+
+    @When("the tool is upgraded")
+    public void theToolIsUpgraded() {
+        tool.upgrade();
+    }
+
+    @Then("the {string} should mine faster")
+    public void theShouldMineFaster(String toolName) {
+        assertTrue(tool instanceof Pickaxe || tool instanceof BreakingTools);
+        assertNotEquals("Tool material should have upgraded", initialMaterial, tool.getMaterial());
+    }
+
+    @Then("the {string} should chop faster")
+    public void theShouldChopFaster(String toolName) {
+        assertTrue(tool instanceof Axe || tool instanceof BreakingTools);
+        assertNotEquals("Tool material should have upgraded", initialMaterial, tool.getMaterial());
+    }
+
+    @Then("the {string} should do more damage")
+    public void theShouldDoMoreDamage(String toolName) {
+        assertTrue(tool instanceof Sword);
+        assertNotEquals("Tool material should have upgraded", initialMaterial, tool.getMaterial());
+    }
+}
---
+++ src/test/resources/features/Items/Tools.feature
@@ -0,0 +1,15 @@
+Feature:  Tools
+  Scenario: Better Pickaxe
+    Given that the player has a "Pickaxe" in "Tools" section in their inventory
+    When the tool is upgraded
+    Then the "Stone Pickaxe" should mine faster
+
+  Scenario: Better Sword
+    Given that the player has a "Sword" in "Tools" section in their inventory
+    When the tool is upgraded
+    Then the "Stone Sword" should do more damage
+
+  Scenario: Better Axe
+    Given that the player has a "Pickaxe" in "Tools" section in their inventory
+    When the tool is upgraded
+    Then the "Stone Axe" should chop faster
