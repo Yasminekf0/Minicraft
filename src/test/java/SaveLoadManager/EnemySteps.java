f8ddfecab7f0e27bc288c0cf660b41f994fb6b50
Stefan Pizlo <s234689@dtu.dk>
2025-05-04 15:30:20 +0200

    EnemySteps, work a little funny though

---
+++ src/test/java/SaveLoadManager/EnemySteps.java
@@ -0,0 +1,46 @@
+package SaveLoadManager;
+
+import controller.entity.EnemyController;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import model.entity.Player;
+import model.entity.mobs.Enemy;
+import model.entity.mobs.MobManager;
+
+public class EnemySteps {
+
+    Enemy enemy = MobManager.getInstance().getEnemies()[0];
+    Player player = Player.getInstance();
+
+    @When("enemy in range of player")
+    public void enemyInRangeOfPlayer() {
+
+
+
+        // Force enemy close enough to trigger pathfinding hopefully...
+        enemy.setWorldPos(player.getWorldPos());
+        enemy.getWorldPos().increment(1, 1);
+    }
+
+    @Then("enemy move towards player")
+    public void enemyMoveTowardsPlayer() {
+        enemy.setOnPath(true);
+        // Works within the game loop and is therefore manually tested
+    }
+
+    @When("enemy touches player")
+    public void enemyTouchesPlayer() {
+        // Should hopefully be able to trigger the damage
+        enemy.setWorldPos(player.getWorldPos());
+        assert(enemy.getOnPath());
+
+
+    }
+
+    @Then("Player take damage")
+    public void playerTakeDamage() {
+        int originalHealth = player.getHealth();
+        enemy.interact();
+        assert player.getHealth() < originalHealth;
+    }
+}
